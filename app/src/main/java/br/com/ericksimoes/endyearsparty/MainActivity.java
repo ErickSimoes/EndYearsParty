32dd09cef32aa0d81f7671a843534480b851518a
Erick Simões <erick.simoes@live.com>
2017-08-15 16:40:46 -0300

    feat: Add navigation between activities

--- app/src/main/java/br/com/ericksimoes/endyearsparty/MainActivity.java
+++ app/src/main/java/br/com/ericksimoes/endyearsparty/MainActivity.java
@@ -1,5 +1,6 @@
 package br.com.ericksimoes.endyearsparty;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -26,7 +27,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View v) {
         int id = v.getId();
         if (id == R.id.button_confirm) {
-            //button confirm clicked
+            Intent intent = new Intent(this, DetailsActivity.class);
+            startActivity(intent);
         }
     }
 
