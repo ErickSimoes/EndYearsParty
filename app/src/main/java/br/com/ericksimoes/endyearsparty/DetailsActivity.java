297a464a2f38002d1b74e1d2e094fdc6afe02c1a
Erick Simões <erick.simoes@live.com>
2017-08-17 17:38:40 -0300

    feat: Add click event on checkbox view component

--- app/src/main/java/br/com/ericksimoes/endyearsparty/DetailsActivity.java
+++ app/src/main/java/br/com/ericksimoes/endyearsparty/DetailsActivity.java
@@ -1,13 +1,33 @@
 package br.com.ericksimoes.endyearsparty;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.CheckBox;
 
-public class DetailsActivity extends AppCompatActivity {
+public class DetailsActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private ViewHolder mViewHolder = new ViewHolder();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
+
+        this.mViewHolder.checkParticipate = (CheckBox) findViewById(R.id.checkbox_participate);
+
+        this.mViewHolder.checkParticipate.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        if (id == R.id.checkbox_participate) {
+            // click action
+        }
+    }
+
+    private static class ViewHolder {
+        CheckBox checkParticipate;
     }
 }
