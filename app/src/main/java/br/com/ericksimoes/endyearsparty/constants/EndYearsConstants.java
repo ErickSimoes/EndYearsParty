79f2adfba059a824c73d522691ead6e2e56ab6c2
Erick Simões <erick.simoes@live.com>
2017-08-17 17:51:44 -0300

    feat: Add Constants file

---
+++ app/src/main/java/br/com/ericksimoes/endyearsparty/constants/EndYearsConstants.java
@@ -0,0 +1,9 @@
+package br.com.ericksimoes.endyearsparty.constants;
+
+/**
+ * Created by erick on 17/08/2017.
+ */
+
+public final class EndYearsConstants {
+    public static final String PRESENCE = "presence_key";
+}
