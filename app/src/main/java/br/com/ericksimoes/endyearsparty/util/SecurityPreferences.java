42af121a9106fbd967a28ed17e6f095f02e58750
Erick Simões <erick.simoes@live.com>
2017-08-17 17:31:24 -0300

    feat: Implement SecurityPreferebces

--- app/src/main/java/br/com/ericksimoes/endyearsparty/util/SecurityPreferences.java
+++ app/src/main/java/br/com/ericksimoes/endyearsparty/util/SecurityPreferences.java
@@ -1,8 +1,24 @@
 package br.com.ericksimoes.endyearsparty.util;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+
 /**
  * Created by erick on 17/08/2017.
  */
 
 public class SecurityPreferences {
+    private final SharedPreferences mSharedPreferences;
+
+    public SecurityPreferences(Context context) {
+        this.mSharedPreferences = context.getSharedPreferences("EndYears", Context.MODE_PRIVATE);
+    }
+
+    public void storeString(String key, String value) {
+        this.mSharedPreferences.edit().putString(key, value).apply();
+    }
+
+    public String getStoredString(String key) {
+        return this.mSharedPreferences.getString(key, "");
+    }
 }
